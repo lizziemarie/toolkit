e4da57941e9ccb1a0bd7b2b86b2040cd729ea2d4
lizzie marie <elizabethmariewillett@gmail.com>
2019-02-27 16:40:56 -0800

    same updates

--- front-page.php
+++ front-page.php
@@ -16,19 +16,10 @@ get_header(); ?>
         $feature_subtitle = get_field('feature_subtitle');
         $download = get_field('download');
         $download_button_text = get_field('download_button_text');
-        $profile_image_1 = get_field('profile_image_1');
-        $profile_image_2 = get_field('profile_image_2');
-        $profile_image_3 = get_field('profile_image_3');
-        $profile_image_4 = get_field('profile_image_4');
-        $profile_image_5 = get_field('profile_image_5');
-        $profile_image_6 = get_field('profile_image_6');
-        $profile_image_7 = get_field('profile_image_7');
-        $profile_image_8 = get_field('profile_image_8');
-        $profile_image_9 = get_field('profile_image_9');
-        $profile_image_10 = get_field('profile_image_10');
-        $profile_image_11 = get_field('profile_image_11');
-        $profile_image_12 = get_field('profile_image_12');
-        $box_icon_1 = get_field('box_icon_1');
+        $download_espanol = get_field('download_espanol');
+				$s2img1 = get_field('s2img1');
+				$s4img1 = get_field('s4img1');
+				$box_icon_1 = get_field('box_icon_1');
         $box_icon_2 = get_field('box_icon_2');
         $box_icon_3 = get_field('box_icon_3');
         $box_icon_4 = get_field('box_icon_4');
@@ -40,12 +31,6 @@ get_header(); ?>
         $box_text_4 = get_field('box_text_4');
         $box_text_5 = get_field('box_text_5');
         $box_text_6 = get_field('box_text_6');
-				$box_color_1 = get_field('box_color_1');
-				$box_color_2 = get_field('box_color_2');
-				$box_color_3 = get_field('box_color_3');
-				$box_color_4 = get_field('box_color_4');
-				$box_color_5 = get_field('box_color_5');
-				$box_color_6 = get_field('box_color_6');
 				$box_link_1 = get_field('box_link_1');
 				$box_link_2 = get_field('box_link_2');
 				$box_link_3 = get_field('box_link_3');
@@ -59,20 +44,13 @@ get_header(); ?>
             <div class="hero-content grid">
               <div>
 								<h2><?php the_field('feature_title'); ?></h2>
-              	<h3><?php the_field('feature_subtitle'); ?></h3>
+              	<h6><?php the_field('feature_subtitle'); ?></h6>
 							</div>
               <p><a href="<?php the_field('download'); ?>" class="btn btnRed btnHero" target="_blank"><?php the_field('download_button_text'); ?></a></p>
+							<p><a href="<?php the_field('download'); ?>" class="" target="_blank">Descargar en Español</a></p>
             </div>
           </div>
 
-          <div class="grid content-container content-grid-2">
-            <div class="social-bar">
-              <?php echo do_shortcode('[Sassy_Social_Share style="background-color:#ffffff;"]') ?>
-            </div>
-            <div class="page-content">
-              <?php the_content(); ?>
-            </div>
-          </div>
 					<div id="gallery">
 						<figure class="slider">
 							<figure>
@@ -102,96 +80,86 @@ get_header(); ?>
 						</figure>
 					</div>
 
-          <div class="grid grid-profile-images">
-            <?php if($profile_image_1) {
-              echo wp_get_attachment_image( $profile_image_1, $size);}
-            ?>
-            <?php if($profile_image_2) {
-              echo wp_get_attachment_image( $profile_image_2, $size);}
-            ?>
-            <?php if($profile_image_3) {
-              echo wp_get_attachment_image( $profile_image_3, $size);}
-            ?>
-            <?php if($profile_image_4) {
-              echo wp_get_attachment_image( $profile_image_4, $size);}
-            ?>
-            <?php if($profile_image_5) {
-              echo wp_get_attachment_image( $profile_image_5, $size);}
-            ?>
-            <?php if($profile_image_6) {
-              echo wp_get_attachment_image( $profile_image_6, $size);}
-            ?>
-            <?php if($profile_image_7) {
-              echo wp_get_attachment_image( $profile_image_7, $size);}
-            ?>
-            <?php if($profile_image_8) {
-              echo wp_get_attachment_image( $profile_image_8, $size);}
-            ?>
-            <?php if($profile_image_9) {
-              echo wp_get_attachment_image( $profile_image_9, $size);}
-            ?>
-            <?php if($profile_image_10) {
-              echo wp_get_attachment_image( $profile_image_10, $size);}
-            ?>
-            <?php if($profile_image_11) {
-              echo wp_get_attachment_image( $profile_image_11, $size);}
-            ?>
-            <?php if($profile_image_12) {
-              echo wp_get_attachment_image( $profile_image_12, $size);}
-            ?>
-          </div>
 
-          <div class="grid grid-color-block">
-            <a href="<?php the_field('box_link_1'); ?>" id="color-block-item-1">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_1'); ?>;">
-                <?php echo $box_text_1; ?>
-								<?php if($box_icon_1) {
-                  echo wp_get_attachment_image( $box_icon_1, $size);}
-                ?>
-              </div>
-            </a>
-            <a href="<?php the_field('box_link_2'); ?>" id="color-block-item-2">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_2'); ?>;">
-                <?php echo $box_text_2; ?>
-								<?php if($box_icon_2) {
-                  echo wp_get_attachment_image( $box_icon_2, $size);}
-                ?>
-              </div>
-            </a>
-            <a href="<?php the_field('box_link_3'); ?>" id="color-block-item-3">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_3'); ?>;">
-                <?php echo $box_text_3; ?>
-								<?php if($box_icon_3) {
-                  echo wp_get_attachment_image( $box_icon_3, $size);}
-                ?>
-              </div>
-            </a>
-            <a href="<?php the_field('box_link_4'); ?>" id="color-block-item-4">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_4'); ?>;">
-                <?php echo $box_text_4; ?>
-								<?php if($box_icon_4) {
-                  echo wp_get_attachment_image( $box_icon_4, $size);}
-                ?>
-              </div>
-            </a>
-            <a href="<?php the_field('box_link_5'); ?>" id="color-block-item-5">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_5'); ?>;">
-                <?php echo $box_text_5; ?>
-								<?php if($box_icon_5) {
-                  echo wp_get_attachment_image( $box_icon_5, $size);}
-                ?>
-              </div>
-            </a>
-            <a href="<?php the_field('box_link_6'); ?>" id="color-block-item-6">
-              <div class="color-block-item grid" style="background-color: <?php the_field('box_color_6'); ?>;">
-                <?php echo $box_text_6; ?>
-								<?php if($box_icon_6) {
-                  echo wp_get_attachment_image( $box_icon_6, $size);}
-                ?>
-              </div>
-            </a>
+					<!-- start of section 2 -->
+          <section class="section-two">
+						<div class="s2img1">
+							<?php if($s2img1) {
+              echo wp_get_attachment_image( $s2img1, $size);}
+            	?>
+						</div>
 
-          </div>
+						<div class="grid s2content">
+            	<div class="s2item1">
+              	<h2 class="underline underline-red">Policing, criminalization, and mass incarceration are primary civil rights issues of our time.</h2>
+            	</div>
+
+            	<div class="s2item2">
+              	<h6>Now more than ever,</h6>
+								<p>tackling discriminatory policing practices and the injustices they perpetuate is critical to achieving racial, gender, economic and social justice, and to bringing us closer to our vision of becoming a society that universally respects, promotes, and protects civil and human rights.</p>
+            	</div>
+
+							<div class="s2item3">
+								<img src=""><a href="<?php the_field('box_link_1'); ?>">Policing by the Numbers</a>
+								<img src=""><a href="<?php the_field('box_link_2'); ?>">Topics of Concern</a>
+							</div>
+
+						</div>
+
+          </section>
+					<!-- end of section 2 -->
+
+					<!-- start of section 3 -->
+					<section class="section-three">
+						<div class="highlight-text-overlay">
+							<h3><span class="highlight highlight-wrapping highlight-teal">We all have a role to play in</span></h3>
+							<h3><span class="highlight highlight-wrapping highlight-teal">resisting infringements on civil</span></h3>
+							<h3><span class="highlight highlight-wrapping highlight-teal">and human rights through</span></h3>
+							<h3><span class="highlight highlight-wrapping highlight-teal">policing practices.</span></h3>
+						<div>
+					</section>
+					<!-- end of section 3 -->
+
+					<!-- start of section 4 -->
+					<section class="section-four">
+						<div class="s4img1">
+							<?php if($s4img1) {
+              echo wp_get_attachment_image( $s4img1, $size);}
+            	?>
+						</div>
+
+						<div class="s4content">
+            	<div class="s4item2">
+              	While politicians, police chiefs and police unions hold <span class="highlight highlight-red highlight-wrapping-inline">significant power</span> over policing, each of us also has the power, individually and collectively, to <span class="highlight highlight-red highlight-wrapping-inline">effect change</span> through grassroots organizing, policy advocacy, and civic engagement.
+            	</div>
+
+							<div class="s4item3">
+								<img src=""><a href="<?php the_field('box_link_1'); ?>">Policing by the Numbers</a>
+								<img src=""><a href="<?php the_field('box_link_2'); ?>">Topics of Concern</a>
+							</div>
+						</div>
+
+          </section>
+					<!-- end of section 4 -->
+
+					<!-- start of section 5 -->
+					<section class="section-five">
+						<div class="highlight-text-overlay">
+							<h3><span class="highlight highlight-wrapping highlight-red">This Toolkit is part of <a href="">The Leadership</a></span></h3>
+							<h3><span class="highlight highlight-wrapping highlight-red"><a href="" class="higlight-link">Conference’s Policing Campaign</a> project.</span></h3>
+						<div>
+						<div class="section-five-text">
+							<div class="">
+              	Visit New Era of Policing: A Guide for Fair, Safe, and Effective Policing for more information about the current state of policing, and recommendations for all levels of police departments to make improvements.
+            	</div>
+							<div class="">
+								<img src=""><a href="<?php the_field('box_link_1'); ?>">Tools for Action</a>
+								<img src=""><a href="<?php the_field('box_link_2'); ?>">Glossary</a>
+							</div>
+
+						</div>
+					</section>
+					<!-- end of section 5 -->
 
           <?php endwhile; // end of the loop. ?>
         </section>
