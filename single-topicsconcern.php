b7c8b38a408b9c9a3cc8af9ac230e8017c6814f8
lizzie marie <elizabethmariewillett@gmail.com>
2019-03-11 14:47:05 -0700

    updated to single toc

--- single-topicsconcern.php
+++ single-topicsconcern.php
@@ -33,13 +33,13 @@ get_header(); ?>
       ?>
         <section>
 
-					<section>
-		        <div>
+					<section class="toc-single">
+		        <div class="page-title">
 		          <h3><span class="highlight highlight-wrapping highlight-red"><?php the_title(); ?></span></h3>
 		        </div>
 
-		        <div>
-		            <a href="<?php the_field('download'); ?>" class="" target="_blank">
+		        <div class="return-toc">
+		            <a href="#" class="" target="_blank">
 									<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/arrow-red.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/arrow-red.png, <?php echo get_stylesheet_directory_uri(); ?>/img/arrow-red@2x.png 2x"class="inline-icon" alt="download">
 									Topics of Concern
 		            </a>
@@ -50,11 +50,19 @@ get_header(); ?>
 
           <section class="content">
 						<div>
-							<h2 class="page-intro"><?php the_field('tertiary_page_intro'); ?></h2>
+							<h2 class="content-intro"><?php the_field('tertiary_page_intro'); ?></h2>
 						</div>
 
-            <div>
+          	<p class="tertiary-download-button">
+								<a href="<?php the_field('download_file_1'); ?>" class="" target="_blank">
+									<?php the_title(); ?>
+									DOWNLOAD SECTION
+									<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/download.png" srcset="<?php echo get_stylesheet_directory_uri(); ?>/img/download.png, <?php echo get_stylesheet_directory_uri(); ?>/img/download@2x.png 2x"class="inline-icon" alt="download">
+								</a>
+						</p>
 
+
+            <div class="accordion-div">
 							<ul class="accordion">
 							  <li>
 							    <input type="checkbox" checked>
@@ -66,27 +74,36 @@ get_header(); ?>
 							    <input type="checkbox" checked>
 							    <i></i>
 							    <h2>Elements of Change</h2>
-							    <div class="accordion-bullet-item">Using the sibling and checked selectors, we can determine the styling of sibling elements based on the checked state of the checkbox input element. One use, as demonstrated here, is an entirely CSS and HTML accordion element. Media queries are used to make the element responsive to different screen sizes.</div>
+							    <div class="accordion-bullet-item"><?php the_field('key_issues_content'); ?></div>
 							  </li>
 							  <li>
 							    <input type="checkbox" checked>
 							    <i></i>
-							    <h2>Third</h2>
-							    <div class="accordion-bullet-item">By making the open state default for when :checked isn't detected, we can make this system accessable for browsers that don't recognize :checked. The fallback is simply an open accordion. The accordion can be manipulated with Javascript (if needed) by changing the "checked" property of the input element.</div>
+							    <h2>Talking Points</h2>
+							    <div class="accordion-bullet-item"><?php the_field('key_issues_content'); ?></div>
+							  </li>
+								<li>
+							    <input type="checkbox" checked>
+							    <i></i>
+							    <h2>Overcoming Opposition</h2>
+							    <div class="accordion-bullet-item"><?php the_field('key_issues_content'); ?></div>
+							  </li>
+								<li>
+							    <input type="checkbox" checked>
+							    <i></i>
+							    <h2>How to Make Change</h2>
+							    <div class="accordion-bullet-item"><?php the_field('key_issues_content'); ?></div>
+							  </li>
+								<li>
+							    <input type="checkbox" checked>
+							    <i></i>
+							    <h2>External Resources</h2>
+							    <div class="accordion-bullet-item"><?php the_field('key_issues_content'); ?></div>
 							  </li>
 							</ul>
-
-
             </div>
 
-            <div class="download-buttons">
-              <p>
-								<a href="<?php the_field('download_file_1'); ?>" class="" target="_blank">
-									<?php the_title(); ?>
-									<?php the_field('download_button_text_1'); ?>
-								</a>
-							</p>
-						</div>
+
 
 						<div class="related">
   		        <h3><?php the_field('related_topics_of_concern_header'); ?></h3>
